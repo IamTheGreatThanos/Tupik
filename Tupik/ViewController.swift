9b4713429f7aa635fe8c931318ef4d1f178a95eb
Dinmukhammed <iamthegreatthanos@gmail.com>
2019-10-12 17:24:53 +0600

    Initial Commit

---
+++ Tupik/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tupik
+//
+//  Created by Mac Book on 10/12/19.
+//  Copyright © 2019 XCode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
